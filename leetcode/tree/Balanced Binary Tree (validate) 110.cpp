131c227952cde047bee7a889f2ded224f7f10b4c
amanbharti0302 <amanbharti0302@gmail.com>
2020-11-13 00:38:10 +0530

    Balanced Binary Tree 110

---
+++ leetcode/tree/Balanced Binary Tree (validate) 110.cpp
@@ -0,0 +1,38 @@
+//Given a binary tree, determine if it is height-balanced.
+
+//==>my approach
+// we can easily find maximum depth of any part of subtree
+//if in any subtree diffrence between maximum depth of left and right subtree exceeds 1 than make boolean false
+//else continue
+
+//==============>my code
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+int cal(TreeNode *root,long long int dep,bool &ch){
+    if(root==NULL){return 0;}
+    int l =cal(root->left,dep+1,ch);
+    int r = cal(root->right,dep+1,ch);   
+    if(abs(r-l)>1)ch=false;
+    return max(l,r)+1;
+}
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL)return true;
+        bool ch=true;
+        cal(root,0,ch);
+        if(ch==false)return false;
+        return true;
+    }
+};
