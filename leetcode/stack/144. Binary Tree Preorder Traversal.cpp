8d0c213f1ffc619a69aa9fb2a40aa4f0e6eb41ab
amanbharti0302 <amanbharti0302@gmail.com>
2021-05-19 12:55:32 +0530

    144. Binary Tree Preorder Traversal

---
+++ leetcode/stack/144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,59 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [1,2]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        stack<TreeNode*>s;
+        
+        while(root!=NULL||!s.empty()){
+            if(root){
+                ans.push_back(root->val);
+                s.push(root);
+                root = root->left;
+            }
+            else{
+                root = s.top();
+                s.pop();
+                root= root->right;
+            }
+        }
+        
+        return ans;
+    }
+};
