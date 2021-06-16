3c7f0ff36141756c73e2be526a0893bc1083e9bd
amanbharti0302 <amanbharti0302@gmail.com>
2021-06-16 15:46:24 +0530

    1753. Maximum Score From Removing Stones

---
+++ leetcode/heap/1753. Maximum Score From Removing Stones.cpp
@@ -0,0 +1,61 @@
+/*
+You are playing a solitaire game with three piles of stones of sizes a​​​​​​, b,​​​​​​ and c​​​​​​ respectively. Each turn you choose two different non-empty piles, take one stone from each, and add 1 point to your score. The game stops when there are fewer than two non-empty piles (meaning there are no more available moves).
+
+Given three integers a​​​​​, b,​​​​​ and c​​​​​, return the maximum score you can get.
+
+ 
+
+Example 1:
+
+Input: a = 2, b = 4, c = 6
+Output: 6
+Explanation: The starting state is (2, 4, 6). One optimal set of moves is:
+- Take from 1st and 3rd piles, state is now (1, 4, 5)
+- Take from 1st and 3rd piles, state is now (0, 4, 4)
+- Take from 2nd and 3rd piles, state is now (0, 3, 3)
+- Take from 2nd and 3rd piles, state is now (0, 2, 2)
+- Take from 2nd and 3rd piles, state is now (0, 1, 1)
+- Take from 2nd and 3rd piles, state is now (0, 0, 0)
+There are fewer than two non-empty piles, so the game ends. Total: 6 points.
+Example 2:
+
+Input: a = 4, b = 4, c = 6
+Output: 7
+Explanation: The starting state is (4, 4, 6). One optimal set of moves is:
+- Take from 1st and 2nd piles, state is now (3, 3, 6)
+- Take from 1st and 3rd piles, state is now (2, 3, 5)
+- Take from 1st and 3rd piles, state is now (1, 3, 4)
+- Take from 1st and 3rd piles, state is now (0, 3, 3)
+- Take from 2nd and 3rd piles, state is now (0, 2, 2)
+- Take from 2nd and 3rd piles, state is now (0, 1, 1)
+- Take from 2nd and 3rd piles, state is now (0, 0, 0)
+There are fewer than two non-empty piles, so the game ends. Total: 7 points.
+*/
+
+
+
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        int ans = 0;
+        int ar[3] = {a,b,c};
+        
+        while(1){
+            if((ar[0]<=0&&ar[1]<=0)||(ar[0]<=0&&ar[2]<=0)||(ar[1]<=0&&ar[2]<=0))break;
+            sort(ar,ar+3);
+            int j = 0;
+            if(ar[j]==0)j++;
+            
+            while(ar[j]>0){
+                sort(ar+j+1,ar+3);
+                ar[j]--;
+                ar[2]--;
+                ans++;
+            }
+            
+        }
+        
+        
+        return ans;
+    }
+};
