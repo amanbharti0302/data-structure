0b8145aeaccc785f316d14317b5e9aca26a27f10
amanbharti0302 <amanbharti0302@gmail.com>
2021-06-17 12:30:02 +0530

    1448. Count Good Nodes in Binary Tree

---
+++ leetcode/tree/1448. Count Good Nodes in Binary Tree.cpp
@@ -0,0 +1,63 @@
+/*
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+Return the number of good nodes in the binary tree.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+Example 2:
+
+
+
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+Example 3:
+
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+void dfs(int &ans,TreeNode *root,int maxm){
+    if(root==NULL)return;
+    if(maxm<=root->val)ans++;
+    maxm = max(root->val,maxm);
+    if(root->left)dfs(ans,root->left,maxm);
+    if(root->right)dfs(ans,root->right,maxm);
+}
+
+
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        int ans = 0;
+        if(root==NULL)return 0;
+        dfs(ans,root,root->val);
+        return ans;
+    }
+};
